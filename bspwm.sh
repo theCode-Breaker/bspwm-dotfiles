dcb192355237aae5a6ca976dd66a61755518f320
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-04-10 13:00:43 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light picom dunst xfce4-power-manager ksuperkey pamixer polkit-gnome i3lock-color xrandr maim viewnior nvim neofetch zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git --needed --noconfirm
+paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light picom dunst xfce4-power-manager ksuperkey pamixer polkit-gnome i3lock-color xrandr maim viewnior nvim neofetch zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git j4-dmenu-desktop --needed --noconfirm
 git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git
 cd bspwm-dotfiles
 cp -R .config/* ~/.config/
