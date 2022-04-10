43206cea67570a2aa0aecb8422e3f9d9982501c5
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-04-10 12:29:12 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,4 +1,5 @@
 paru -S bspwm sxhkd feh polybar xorg-xsetroot picom dunst xfce4-power-manager ksuperkey polkit-gnome --needed --noconfirm
+git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git
 cd bspwm-dotfiles
 cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
