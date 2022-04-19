8f845e34e7821f138756c2a4075f49d5dfa18ed4
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-04-19 12:39:21 +0530

    neovim update

--- bspwm.sh
+++ bspwm.sh
@@ -2,8 +2,12 @@
 sudo pacman -Syu --needed git base-devel
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -si
-paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey gtop htop ncdu nerd-fonts-jetbrains-mono pamixer polkit-gnome i3lock-color nvim fm6000 fortune-mod network-manager-applet zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
-git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git
+paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey gtop htop ncdu nerd-fonts-jetbrains-mono pamixer polkit-gnome i3lock-color nvim fm6000 fortune-mod network-manager-applet nvim zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git --depth 1
 cd bspwm-dotfiles
 cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
