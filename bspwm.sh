3379db38991a5f46c97cabd7c1a0860c4a7e46dd
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-05-04 11:51:24 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu --needed git base-devel
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -si
-paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey adobe-source-code-pro-fonts nerd-fonts-jetbrains-mono ttf-iosevka-nerd polkit-gnome i3lock-color fm6000 fortune-mod network-manager-applet helix zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey nerd-fonts-jetbrains-mono polkit-gnome i3lock-color fm6000 fortune-mod network-manager-applet helix zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
 git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git --depth 1
 cd bspwm-dotfiles
 cp -R .config/* ~/.config/
