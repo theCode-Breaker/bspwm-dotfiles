53dbdbb82d74a8a2a7aa868b88fd643788937d1d
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-05-08 10:37:57 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu --needed git base-devel
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -si
-paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome i3lock-color fm6000 fortune-mod network-manager-applet helix zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome i3lock-color fm6000 fortune-mod network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
 git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git --depth 1
 cd bspwm-dotfiles
 cp -R .config/* ~/.config/
@@ -12,3 +12,5 @@ cp .zshrc-personal ~
 mkdir ~/.local/bin
 cp -R .local/bin/* ~/.local/bin
 chmod -R +x ~/.local/bin
+betterlockscreen -u ~/.config/bspwm/backgrounds/evening-sky.png
+sudo systemctl enable --now betterlockscreen@$USER.service
